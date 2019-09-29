0f17785d7f979b324e18eb70467ae7c63616db1a
DuanXu-97 <956378834@qq.com>
2019-09-29 15:39:11 +0800

    add files

--- RubikCubeWebApp/wsgi.py
+++ RubikCubeWebApp/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append("/var/www/RubikCubeWebApp/")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RubikCubeWebApp.settings")
 
