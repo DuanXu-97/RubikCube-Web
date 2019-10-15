9ff755c91d8ea87c4120088319f23f2d90e530b1
DuanXu-97 <956378834@qq.com>
2019-10-15 18:24:25 +0800

    修改UI，增加选择算法按钮

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -130,41 +130,45 @@
 	  state: cube.getState(),
 	  alg: util.getQueryParameter('algorithm') || '',
 	  button: function () {}, // used for other buttons
+	  algorithm: '',
 	}
 
-	function initGui () {
-	  var c = folder('Cube')
-	  c.add(controls, 'size').min(1).step(1).name('Size')
-	    .onChange(function (size) { cube.setSize(size); })
-	  c.add(controls, 'scramble').name('Scramble')
-	    .onChange(function () { cube.scramble(); })
-	  c.add(controls, 'solve').name('Solve')
-	    .onChange(function () { solve(); })
+	var algorithms = ['公式法', '最短路径法'];
 
-	  var v = folder('View')
-	  v.add(controls, 'labels').name('Show Labels').listen()
+	function initGui () {
+	  var c = folder('操作')
+	//  c.add(controls, 'size').min(1).step(1).name('Size')
+	//    .onChange(function (size) { cube.setSize(size); })
+	  c.add(controls, 'scramble').name('随机打乱')
+	    .onChange(function () { cube.scramble(); })
+	  c.add(controls, 'solve').name('解魔方')
+	    .onChange(function () { solve(controls.algorithm); })
+	  c.add(controls, 'algorithm', algorithms).setValue(algorithms[1]).name('算法')
+
+	  var v = folder('视角')
+	  v.add(controls, 'labels').name('显示方位标记').listen()
 	    .onFinishChange(function (v) { cube.setLabels(v); })
-	  v.add(controls, 'camera').name('Reset Camera')
+	  v.add(controls, 'camera').name('重置摄像机')
 	    .onFinishChange(function () { cube.resetCamera(); })
 
-	  var a = folder('Animation')
-	  var interpolators = Object.keys(interpolation.interpolators)
-	  a.add(controls, 'duration').min(0).step(100).setValue(300).name('Duration (ms)')
-	    .onFinishChange(function (d) { cube.setAnimationDuration(d); })
-	  a.add(controls, 'interpolator', interpolators).setValue(interpolators[0]).name('Interpolator')
-	    .onFinishChange(function (i) { cube.setInterpolator(i); })
+	//  var a = folder('Animation')
+	//  var interpolators = Object.keys(interpolation.interpolators)
+	//  a.add(controls, 'duration').min(0).step(100).setValue(300).name('Duration (ms)')
+	//    .onFinishChange(function (d) { cube.setAnimationDuration(d); })
+	//  a.add(controls, 'interpolator', interpolators).setValue(interpolators[0]).name('Interpolator')
+	//    .onFinishChange(function (i) { cube.setInterpolator(i); })
 
-	  var st = folder('State')
-	  st.add(controls, 'state').name('Modify State').listen()
-	  st.add(controls, 'button').name('Apply State')
-	    .onFinishChange(function () { cube.setState(controls.state); })
+	//  var st = folder('State')
+	//  st.add(controls, 'state').name('Modify State').listen()
+	//  st.add(controls, 'button').name('Apply State')
+	//    .onFinishChange(function () { cube.setState(controls.state); })
 
-	  var alg = folder('Algorithm')
-	  alg.add(controls, 'alg').name('Edit Algorithm').listen()
-	  alg.add(controls, 'button').name('Run')
-	    .onFinishChange(function () { cube.algorithm(controls.alg); })
-	  alg.add(controls, 'button').name('Invert')
-	    .onFinishChange(function () { controls.alg = algorithm.invert(controls.alg); })
+	//  var alg = folder('算法')
+	//  alg.add(controls, 'alg').name('Edit Algorithm').listen()
+	//  alg.add(controls, 'button').name('Run')
+	//    .onFinishChange(function () { cube.algorithm(controls.alg); })
+	//  alg.add(controls, 'button').name('Invert')
+	//    .onFinishChange(function () { controls.alg = algorithm.invert(controls.alg); })
 
 	  if (window.innerWidth <= 500) gui.close()
 
@@ -183,11 +187,19 @@
 	  canvas.focus()
 	}
 
-	function solve () {
-	  var alg = solver.solve(new State(cube.getState()))
-	  var opt = algorithm.optimize(alg)
-	  cube.algorithm(opt)
-	  console.log('Algorithm:', alg)
+	function solve (selectedAlg) {
+	  if (selectedAlg == algorithms[0]) {
+	//    alert('公式法');
+	    var alg = solver.solve(new State(cube.getState()))
+	    var opt = algorithm.optimize(alg)
+	    cube.algorithm(opt)
+	    console.log('Algorithm:', alg)
+	  }
+
+	  else if (selectedAlg == algorithms[1]) {
+	//    alert('最短路径法');
+	  }
+	  else alert('Error Algorithm!');
 	}
 
 	module.exports = cube
@@ -758,9 +770,9 @@
 	      if (rc != undefined) {
 	        var face = rc.face
 	        if (face !== undefined) {
-	          if (e.button == 2)
+	          if (e.button == 0)
 	              self.onCubieClick(rc.face, rc.object, e.clientX, e.clientY)
-	          else if (e.button == 0) {
+	          else if (e.button == 2) {
 	              self.algorithm(face)
 	          }
 	        }
--- webpack/assets/cube.js
+++ webpack/assets/cube.js
@@ -438,9 +438,9 @@ Cube.prototype._init = function _init () {
       if (rc != undefined) {
         var face = rc.face
         if (face !== undefined) {
-          if (e.button == 2)
+          if (e.button == 0)
               self.onCubieClick(rc.face, rc.object, e.clientX, e.clientY)
-          else if (e.button == 0) {
+          else if (e.button == 2) {
               self.algorithm(face)
           }
         }
