45fd48d5c7bd450bf11290c29a3628432e1e673f
DuanXu-97 <956378834@qq.com>
2019-11-15 11:20:52 +0800

    test

--- RubikCubeWebApp/kernel/scripts/config.py
+++ RubikCubeWebApp/kernel/scripts/config.py
@@ -5,6 +5,6 @@ class Config:
         self.model_loc = '../savedModels/cube3/1/'
         self.model_name = 'model.meta'
         self.bfs = 0
-        self.nnet_parallel = 100
+        self.nnet_parallel = 500
         self.depth_penalty = 0.2
         self.verbose = 'store_true'
--- RubikCubeWebApp/test.py
+++ RubikCubeWebApp/test.py
@@ -1,6 +1,15 @@
 import numpy as np
+import pickle
 
-color = np.array([1,1,1])
-color.copy()
-print(color.shape)
-print(type(color))
+# color = np.array([1,1,1])
+# color.copy()
+# print(color.shape)
+# print(type(color))
+
+filename = '../states.pkl'
+inputData = pickle.load(open(filename,"rb"), encoding='utf-8')
+
+states = inputData['states']
+
+print(states)
+
