6898df513fd86e927f9b6f7296beba98092f7bae
fgksgf <fgksgf@yahoo.com>
2019-11-29 15:48:13 +0800

    增加单元测试

--- RubikCubeWebApp/calculate_states/tests/test_cube_string.py
+++ RubikCubeWebApp/calculate_states/tests/test_cube_string.py
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
 from unittest import TestCase
+
+from RubikCubeWebApp.calculate_states.cube_string import CubeString
 
 
 class TestCubeString(TestCase):
+    """CubeString类单元测试"""
+
     def test_reorder(self):
-        input = 'UUUUUUUUULLLLLLLLLFFFFFFFFFRRRRRRRRRBBBBBBBBBDDDDDDDDD'
-        expect_1 = ''
-        self.fail()
+        input_str = 'UUUUUUUUULLLLLLLLLFFFFFFFFFRRRRRRRRRBBBBBBBBBDDDDDDDDD'
+        test_cases = {
+            'UDLRBF': 'UUUUUUUUUDDDDDDDDDLLLLLLLLLRRRRRRRRRBBBBBBBBBFFFFFFFFF',
+            'UDLBFR': 'UUUUUUUUUDDDDDDDDDLLLLLLLLLBBBBBBBBBFFFFFFFFFRRRRRRRRR',
+            'DLBFRU': 'DDDDDDDDDLLLLLLLLLBBBBBBBBBFFFFFFFFFRRRRRRRRRUUUUUUUUU',
+            'BFRUDL': 'BBBBBBBBBFFFFFFFFFRRRRRRRRRUUUUUUUUUDDDDDDDDDLLLLLLLLL'
+        }
+        for order, expect in test_cases.items():
+            self.assertEqual(CubeString(input_str).reorder(ordering=order), expect)
