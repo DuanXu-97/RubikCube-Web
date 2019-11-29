a40707e07586e60b98a52e6e62cdd48d0a81ca17
DuanXu-97 <956378834@qq.com>
2019-11-29 18:35:33 +0800

    debug

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -293,7 +293,7 @@
 	        if (data.code == '1') {
 
 	          swal({
-	            title: "正在求解中...",
+	            text: "正在求解中...",
 	            showConfirmButton: false,
 	            showLoaderOnConfirm: true,
 	            imageUrl: "/static/img/loading.gif",
@@ -320,7 +320,7 @@
 	                }
 	                else{
 	                  swal({
-	                    text: data.message,
+	                    text: "求解失败，请重试",
 	                    type: "error"
 	                  });
 	                }
@@ -329,7 +329,7 @@
 	        }
 	        else if (data.code == '2') {
 	          swal({
-	            text: data.message,
+	            text: "该状态搜索时间较长，已转为公式法",
 	            type: "info",
 	          }).then(function() {
 	              controls.algorithm = algorithms[0]
@@ -346,7 +346,7 @@
 	        }
 	        else{
 	          swal({
-	            text: data.message,
+	            text: "魔方状态不合法",
 	            type: "error"
 	          });
 	        }
