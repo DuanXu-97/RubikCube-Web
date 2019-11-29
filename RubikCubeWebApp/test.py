9c2b9f41390d85c8cbcf22dcf91bb61cdd0cc487
DuanXu-97 <956378834@qq.com>
2019-11-29 14:35:34 +0800

    添加deepcubea状态表示转换脚本

--- RubikCubeWebApp/test.py
+++ RubikCubeWebApp/test.py
@@ -1,18 +1,20 @@
-# import numpy as np
-# import pickle
-#
-# # color = np.array([1,1,1])
-# # color.copy()
-# # print(color.shape)
-# # print(type(color))
-#
-# filename = '../states.pkl'
-# inputData = pickle.load(open(filename,"rb"), encoding='utf-8')
-#
-# states = inputData['states']
-#
-# print(states)
+import os
+import pickle
+from settings import *
+from calculate_states.main import convert_states_to_str
 
-list = [1,2]
-print(str(list))
+with open(os.path.join(BASE_DIR, 'RubikCubeWebApp/solver/deepcubea/states.pkl'), "rb") as f:
+    inputData = pickle.load(f)
+    deepcubea_states = inputData['states']
+
+state_by_face_list = []
+for state in deepcubea_states:
+    state_by_face = convert_states_to_str(state)
+    print(state_by_face)
+    state_by_face_list.append(state_by_face)
+
+print(state_by_face_list)
+
+with open(os.path.join(BASE_DIR, 'RubikCubeWebApp/solver/deepcubea/states_by_face.pkl'), 'wb') as f_w:
+    pickle.dump(state_by_face_list, f_w)
 
